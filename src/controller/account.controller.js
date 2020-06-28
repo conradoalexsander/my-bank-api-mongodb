461a05e9c889dadf3e94371d318b7e772e0d7342
Conrado Alexsander <conradoalexsander@hotmail.com>
2020-06-28 03:25:12 -0300

    Added new routes

--- src/controller/account.controller.js
+++ src/controller/account.controller.js
@@ -13,8 +13,7 @@ const accountController = (() => {
     return account;
   };
 
-  const withdraw = async (agencia, conta, value) => {
-    const tax = 1;
+  const withdraw = async (agencia, conta, value, tax) => {
     let account = await accountModel.findOne({ conta, agencia });
 
     if (!account) {
@@ -49,12 +48,111 @@ const accountController = (() => {
     return accounts;
   };
 
+  const doTransferAccounts = async (originAccount, destinyAccount, value) => {
+    const payer = await accountModel.findOne({ conta: originAccount });
+    const receiver = await accountModel.findOne({ conta: destinyAccount });
+
+    if (!payer || !receiver) {
+      return null;
+    }
+
+    let {
+      conta: payerAccount,
+      agencia: payerAgency,
+      balance: payerBalance,
+    } = payer;
+
+    let {
+      conta: receiverAccount,
+      agencia: receiverAgency,
+      balance: receiverBalance,
+    } = receiver;
+
+    const tax = 8;
+
+    if (payer.agencia == receiver.agencia) {
+      const withdrawedPayer = await withdraw(
+        payerAgency,
+        payerAccount,
+        value,
+        0
+      );
+      deposit(receiverAgency, receiverAccount, value);
+
+      const { conta, agencia, balance } = withdrawedPayer;
+
+      return { conta, agencia, balance };
+    } else if (payer.agencia !== receiver.agencia) {
+      const withdrawedPayer = await withdraw(
+        payerAgency,
+        payerAccount,
+        value,
+        tax
+      );
+      deposit(receiverAgency, receiverAccount, value);
+
+      const { conta, agencia, balance } = withdrawedPayer;
+
+      return { conta, agencia, balance };
+    } else {
+      return null;
+    }
+  };
+
+  const averageBalanceInAgency = async (agencia) => {
+    const accountsInAgency = await accountModel.find({ agencia });
+
+    if (accountsInAgency.length <= 0) {
+      return null;
+    }
+
+    const average =
+      accountsInAgency.reduce((acc, account) => {
+        console.log(account);
+        return acc + account.balance;
+      }, 0) / accountsInAgency.length;
+
+    return { agencia: agencia, saldo_medio: average.toFixed(2) };
+  };
+
+  const lowestBalances = async (quantity) => {
+    let accounts = await accountModel
+      .find({}, null, { sort: { balance: 1 } })
+      .limit(quantity);
+
+    if (accounts.length <= 0) {
+      return null;
+    }
+    return accounts;
+  };
+
+  const highestBalances = async (quantity) => {
+    let accounts = await accountModel
+      .find({}, null, { sort: { balance: -1 } })
+      .limit(quantity);
+
+    if (accounts.length <= 0) {
+      return null;
+    }
+    return accounts;
+  };
+
+  // const transferToPrivate = async () => {
+  //   let accounts = await accountModel.aggregate([
+  //     { $match: { agencia: '$agencia' } },
+  //   ]);
+  // };
+
   return {
     deposit,
     withdraw,
     show,
     //destroy,
     listActiveAccountsInAgency,
+    doTransferAccounts,
+    averageBalanceInAgency,
+    lowestBalances,
+    highestBalances,
   };
 })();
 
--- src/routes/accountsRoutes.js
+++ src/routes/accountsRoutes.js
@@ -37,7 +37,7 @@ accountsRouter.put('/account/withdraw', async (req, res) => {
   try {
     const { agencia, conta, value } = req.body;
 
-    let account = await accountController.withdraw(agencia, conta, value);
+    let account = await accountController.withdraw(agencia, conta, value, 1);
     if (!account) {
       res
         .status(404)
@@ -88,5 +88,89 @@ accountsRouter.delete('/account/destroy', async (req, res) => {
     res.status(500).send({ error: error.message });
   }
 });
+
+accountsRouter.put('/account/transfer', async (req, res) => {
+  try {
+    const {
+      conta_origem: contaOrigem,
+      conta_destino: contaDestino,
+      value,
+    } = req.body;
+
+    let account = await accountController.doTransferAccounts(
+      contaOrigem,
+      contaDestino,
+      value
+    );
+    if (!account) {
+      res
+        .status(404)
+        .send({ error: 'No account could be found with this criteria' });
+    }
+
+    res.send(account);
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+});
+
+accountsRouter.get('/agency/:agencia/average', async (req, res) => {
+  try {
+    const { agencia } = req.params;
+
+    const averagebalance = await accountController.averageBalanceInAgency(
+      agencia
+    );
+
+    if (!averagebalance) {
+      res
+        .status(404)
+        .send({ error: 'No agency could be found with this criteria' });
+    }
+
+    res.send(averagebalance);
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+});
+
+accountsRouter.get('/account/lowest-balances/:quantidade', async (req, res) => {
+  try {
+    const { quantidade } = req.params;
+
+    const lowestBalances = await accountController.lowestBalances(
+      Number(quantidade)
+    );
+
+    if (!lowestBalances) {
+      res.status(404).send({ error: 'No accounts could be found ' });
+    }
+
+    res.send(lowestBalances);
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+});
+
+accountsRouter.get(
+  '/account/highest-balances/:quantidade',
+  async (req, res) => {
+    try {
+      const { quantidade } = req.params;
+
+      const highestBalances = await accountController.highestBalances(
+        Number(quantidade)
+      );
+
+      if (!highestBalances) {
+        res.status(404).send({ error: 'No accounts could be found ' });
+      }
+
+      res.send(highestBalances);
+    } catch (error) {
+      res.status(500).send({ error: error.message });
+    }
+  }
+);
 
 export default accountsRouter;
